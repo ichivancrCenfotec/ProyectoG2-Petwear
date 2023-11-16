0788bb1072d09c97f30befb7b9ac7bcc6e990136
Ivan Wu Cen <iwuc@ucenfotec.ac.cr>
2023-11-16 15:49:43 -0600

    Modificación a UserCrudFactory

--- DataAccess/UserCrudFactory.cs
+++ DataAccess/UserCrudFactory.cs
@@ -9,33 +9,17 @@ using System.Threading.Tasks;
 
 namespace DataAccess
 {
-    public class UsuarioCrudFactory : CrudFactory
+    public class UserCrudFactory : CrudFactory
     {
 
-      public UsuarioCrudFactory()
+        public UserCrudFactory()
         {
             _dao = SqlDao.GetInstance();
         }
 
-
         public override void Create(BaseDTO baseDTO)
         {
-            //Conversion del baseDTO al user
-            var user = baseDTO as User;
-
-            //vamos a definir el SqlOperation para esta operación de creación
-            var SqlOperation = new SqlOperation { ProcedureName = "CRE_USER_PR" };
-            SqlOperation.AddVarcharParam("P_NAME", user.Name);
-            SqlOperation.AddVarcharParam("P_LASTNAME", user.LastName);
-            SqlOperation.AddVarcharParam("P_PASSWORD", user.Password);
-            SqlOperation.AddVarcharParam("P_EMAIL", user.Email);
-            SqlOperation.AddVarcharParam("P_ADDRESS", user.Address);
-            SqlOperation.AddVarcharParam("P_ROLE", user.Role);
-            SqlOperation.AddIntParam("P_PHONENUMBER", user.PhoneNumber);
-
-            //Invocamos al SQLDao y le indicamos que ejecute el SP
-            _dao.ExecuteProcedure(SqlOperation);
-
+            throw new NotImplementedException();
         }
 
         public override void Delete(BaseDTO baseDTO)
@@ -50,70 +34,17 @@ namespace DataAccess
 
         public override List<T> RetrieveAll<T>()
         {
-
-            var lstUsers = new List<T>();
-
-            var sqlOperation = new SqlOperation { ProcedureName = "RET_ALL_USERS_PR" };
-
-            //Devuelve la lista de diccionarios
-            var lstResults = _dao.ExecuteQueryProcedure(sqlOperation);
-
-            if (lstResults.Count > 0)
-            {
-                foreach (var row in lstResults)
-                {
-                    var userDTO = BuildUser<T>(row);
-                    lstUsers.Add(userDTO);
-                }
-            }
-
             throw new NotImplementedException();
-
-            if (lstResults.Count > 0)
-            {
-                foreach (var row in lstResults)
-                {
-                    var userDTO = BuildUser<T>(row);
-                    lstUsers.Add(userDTO);
-                }
-            }
-
         }
 
-        private T BuildUser<T>(Dictionary<string, object> row)
+        public override T RetrieveById<T>(int id)
         {
             throw new NotImplementedException();
-
         }
-
-    
-
-        public T RetrieveByEmail<T>(int id)
-        {
-            //extraemos el primer valor de la lista
-            var row = lstResults[0]; //solo hay 1 resultado
-
-            //contruir el objeto
-            var userDTO = BuildUser<T>(row);
-            return userDTO;
-
-        }
-        return default(T);
-
-    }
-
-
-        public T RetrieveByEmail<T>(int Id) => throw new NotImplementedException();
-
 
         public override void Update(BaseDTO baseDTO)
         {
             throw new NotImplementedException();
         }
-
-<<<<<<< HEAD
     }
 }
-=======
-
->>>>>>> a77c21485cc05a4f66f726d0002e027e17ce790a
