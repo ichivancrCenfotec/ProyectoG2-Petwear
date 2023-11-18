fd5605f4af6049c9bf89d8dab4760663b0b65ead
Ivan Wu Cen <iwuc@ucenfotec.ac.cr>
2023-11-18 10:55:19 -0600

    Pantalla de Log in

---
+++ WebApp/Pages/LogIn.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class LogInModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
