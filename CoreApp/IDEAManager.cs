b38e7e51ca05891b622f7a3291a1bcb3e91e1073
montero0423 <emonteroc@ucenfotec.ac.cr>
2023-11-24 22:58:21 -0600

    IdeaBoard 2.0

--- CoreApp/IDEAManager.cs
+++ CoreApp/IDEAManager.cs
@@ -10,7 +10,17 @@ namespace CoreApp
 {
     public class IDEAManager
     {
+        public void Create(PetData pd)
+        {
+            var pdf = new IDEACrudFactory();
+            pdf.Create(pd);
+        }
         public object? RetrieveAll()
+        {
+            var uc = new IDEACrudFactory();
+            return uc.RetrieveAll<PetData>();
+        }
+        public object? RetrieveById(PetData pet) 
         {
             var uc = new IDEACrudFactory();
             return uc.RetrieveAll<PetData>();
