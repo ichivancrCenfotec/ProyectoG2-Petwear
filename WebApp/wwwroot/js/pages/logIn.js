61764fe6d0d42020f93d68b0b36a54e5891f2e9f
Ivan Wu Cen <iwuc@ucenfotec.ac.cr>
2023-12-02 10:58:23 -0600

    Log in

--- WebApp/wwwroot/js/pages/logIn.js
+++ WebApp/wwwroot/js/pages/logIn.js
@@ -37,24 +37,17 @@ function UsersController() {
         var serviceRoute2 = this.ApiService + "/RetrieveByEmail";
 
 
+       // ctrlActions.GetToApi2(serviceRoute2, users.email){ };
+
+
         ctrlActions.PostToAPI(serviceRoute, users, function () {
             console.log("User logged in ---> " + JSON.stringify(users))
 
-            fetch(serviceRoute2)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    var aux = $.parseJSON(response.json());
-                    console.log(aux)
-                    sessionStorage.setItem('role', aux.Role);
-                    sessionStorage.setItem('photo', aux.Photo);
 
-                })
 
             
             //Redireccionar a la pagina de inicio
-            window.location.href = "https://localhost:7298/Index"; 
+          //  window.location.href = "https://localhost:7298/Index"; 
 
             //window.location.href = "https://localhost:7298/UserDashboard"; ESTO LO COMENTÉ XQ NO EXISTE LA PAG AÚN!
 
